fc892a88ff969f4c89504db4fae66db3d8104bee
composercordero <composercordero@gmail.com>
2023-05-15 21:40:38 -0500

    Add display function
    Buttons now show numbers and operators in the display

--- script.js
+++ script.js
@@ -1,19 +1,63 @@
 //buttons and display
 
-const clearBtn = document.getElementsByClassName('clear');
-const pointBtn = document.getElementsByClassName('point');
-const equalBtn = document.getElementsByClassName('equal');
-const plusMinusBtn = document.getElementsByClassName('plus-minus');
-const percentagesBtn = document.getElementsByClassName('percentages');
+const clearBtn = document.getElementById('clear');
+const pointBtn = document.getElementById('point');
+const equalBtn = document.getElementById('equal');
+const plusMinusBtn = document.getElementById('plus-minus');
+const percentagesBtn = document.getElementById('percentages');
 
 const numberBtn = document.querySelectorAll('[data-number]');
 const operatorBtn = document.querySelectorAll('[data-operator]');
-const display = document.getElementsByClassName('display');
+let displayScreen = document.getElementById('display');
 
-//functions
+//operator function
 
 const add = function(a,b) {return a + b};
 const subtract = function(a,b) {return a - b};
 const multiply = function(a,b) {return a * b};
 const divide = function(a,b) {return a / b};
 const percentage = function(a) {return a/100};
+
+//Actions
+clearBtn.addEventListener('click', clearAll);
+// pointBtn.addEventListener('click', appendNumber(point.textContent));
+equalBtn.addEventListener('click', result);
+plusMinusBtn.addEventListener('click', plusMinus);
+percentagesBtn.addEventListener('click', percentage);
+numberBtn.forEach((button) => button.addEventListener('click', () => appendNumber(button.textContent))
+)
+
+operatorBtn.forEach((button) => button.addEventListener('click', () => appendOperator(button.textContent))
+)
+
+//variables
+
+let firstNumber;
+let operator;
+let secondNumber;
+
+//display function
+
+function appendNumber(a) {
+    if (displayScreen.textContent === '0') {displayScreen.textContent = a
+    }else{
+        displayScreen.textContent += a
+  }}
+
+function appendOperator(a) {
+if (displayScreen.textContent === '0') {displayScreen.textContent = a
+}else{
+    displayScreen.textContent += a
+}}
+
+function clearAll(){
+    console.log('clear')
+}
+
+function result(){
+    console.log('result')
+}
+
+function plusMinus(){
+    console.log('plusMinus')
+}
